828abb4ad7c8d894a0a76c736b9f072c44c3139f
Aranoxx <aranoxx@hotmail.com>
2019-04-02 08:03:56 +0200

    minor

---
+++ src/app/ExpLog.java
@@ -0,0 +1,20 @@
+package app;
+
+import javax.swing.JFrame;
+
+public class ExpLog extends JFrame {
+	
+	public void ExpLog() {
+		
+		initFrame();
+		
+	}
+
+	public static void main(String[] args) {
+		
+	}
+
+	public void initFrame() {
+		
+	}
+}
